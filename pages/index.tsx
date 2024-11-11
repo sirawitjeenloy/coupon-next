00ada8943dbb86bf6dfa46efc8a441b61d6428f3
sirawitjeenloy <sirawitjeenloy@gmail.com>
2024-11-11 13:26:50 +0700

    add redirect to coupon page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import Head from 'next/head'
-import { Typography } from 'antd'
+import { Skeleton, Typography } from 'antd'
+import React from 'react'
+import { useRouter } from 'next/router'
 
 
 const {Text} = Typography
 
 export default function Home() {
+  const router = useRouter()
+  
+  React.useEffect(() => {
+    router.push({pathname: '/coupon'})
+  }, [router])
   return (
-    <>
-      <Text>Test</Text>
-    </>
+    <div style={{display: 'flex', flexDirection: 'column', width: '100%', height: '1vh'}}>
+      <Skeleton />
+    </div>
   )
 }
